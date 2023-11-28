6c60ac0a477b84b42b166a7a1515c9ce67c25640
Nabil <bekkanabil06@gmail.com>
2023-11-28 17:30:51 +0100

    Fix page redirect error

--- pages/projectsPage/[project].tsx
+++ pages/projectsPage/[project].tsx
@@ -4,6 +4,7 @@ import { GetStaticPropsContext } from "next";
 import Image from "next/image";
 import { useContext } from "react";
 import styles from "@/styles/ProjectPage.module.css";
+import { useRouter } from "next/router";
 
 type Props = {
     project: ProjectType;
@@ -11,33 +12,39 @@ type Props = {
 
 export default function ProjectDetails({project}:Props) {
     const {language} = useContext(LanguageContext);
-    const {name, desription, imgUrl, url, urlGithub, technologies} = project;
-    return (
-        <main className={styles.project}>
-            <h2>{name}</h2>
-            <div className={styles.imgs}>
-                {imgUrl.map((url: string, index) => <Image 
-                    key={index}
-                    src={url}
-                    alt={`${name} image ${index}`}
-                    width={300}
-                    height={300}
-                />)}
-            </div>
-            <div className={styles.descriptionDiv}>
-                <h3>Description</h3>
-                <p>{language === "fr" ? desription.fr : desription.en}</p>
-            </div>
-            <div className={styles.technologiesDiv}>
-                <h3>Technologies</h3>
-                <p>{technologies}</p>
-            </div>
-            <div className={styles.viewContent}>
-                {url && <a href={url} target="_blanc">WEBSITE</a>}
-                <a href={urlGithub} target="_blanc">{language === "fr" ? "CODE SOURCE" : "SOURCE CODE"}</a>
-            </div>
+    if (project){
+        const {name, desription, imgUrl, url, urlGithub, technologies} = project;
+        return (
+            <main className={styles.project}>
+                <h2>{name}</h2>
+                <div className={styles.imgs}>
+                    {imgUrl.map((url: string, index) => <Image 
+                        key={index}
+                        src={url}
+                        alt={`Image ${index}`}
+                        width={300}
+                        height={300}
+                    />)}
+                </div>
+                <div className={styles.descriptionDiv}>
+                    <h3>Description</h3>
+                    <p>{language === "fr" ? desription.fr : desription.en}</p>
+                </div>
+                <div className={styles.technologiesDiv}>
+                    <h3>Technologies</h3>
+                    <p>{technologies}</p>
+                </div>
+                <div className={styles.viewContent}>
+                    {url && <a href={url} target="_blanc">WEBSITE</a>}
+                    <a href={urlGithub} target="_blanc">{language === "fr" ? "CODE SOURCE" : "SOURCE CODE"}</a>
+                </div>
+            </main>
+        )
+    } else {
+        return <main className={styles.project}>
+            <h2 style={{color: "red"}}>Project not found!</h2>
         </main>
-    )
+    }
 }
 
 export const getStaticProps = async (context: GetStaticPropsContext) => {
@@ -46,7 +53,6 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
     if (context.params?.project){
         const projectSelected = context.params?.project as string;
         const project = datas.myProjects.find((project: ProjectType) => project.id === projectSelected) as ProjectType;
-
         if(project){
             return {
                 props: {
